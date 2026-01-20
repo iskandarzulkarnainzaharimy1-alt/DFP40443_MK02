ca0a040f695b9ed08698acc52391b15beabfac30
iskandar  zulkarnain <iskandarzulkarnain.zaharimy1@gmail.com>
2026-01-20 15:01:32 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,24 @@
+
+<?php
+$nama=  "ISKANDAR  ZULKARNAIN  BIN ZAHARIMY";
+$nomatrik="18DIT24F1140";
+$kelas="DIT4D";
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Maklumat Pelajar</title>
+</head>
+<body>
+
+<h1>Maklumat Pelajar</h1>
+
+<img src="gambar.jpeg" alt="foto saya" width="200">
+
+<p>Nama: <?= $nama ?></p>
+<p>No Matrik: <?= $nomatrik ?></p>
+<p>Kelas: <?= $kelas ?></p>
+
+</body>
+</html>
